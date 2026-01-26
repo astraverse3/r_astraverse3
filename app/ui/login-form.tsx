5de10ea311ad8a13e37cd3fe7116d9f78dba2e35
astraverse3 <astraverse3@gmail.com>
2026-01-26 17:23:08 +0900

    Lower client-side password minLength to 4

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -44,7 +44,7 @@ export default function LoginForm() {
                             name="password"
                             placeholder="Enter password"
                             required
-                            minLength={6}
+                            minLength={4}
                         />
                         <Lock className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-slate-400 peer-focus:text-blue-500" />
                     </div>
